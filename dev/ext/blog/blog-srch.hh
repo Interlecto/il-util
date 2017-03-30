8287528e8bc4cfd6d278b1a4b0447d04db1f09d1
chlewey <chlewey@gmail.com>
2017-03-29 22:49:14 -0500

    pass 1

---
+++ dev/ext/blog/blog-srch.hh
@@ -0,0 +1,6 @@
+#ifndef __BLOG_SRCH_HH__
+#define __BLOG_SRCH_HH__
+#include "blog.hh"
+#include "../search/search.hh"
+
+#endif
---
+++ dev/ext/blog/blog.hh
@@ -0,0 +1,4 @@
+#ifndef __BLOG_HH__
+#define __BLOG_HH__
+#include "../../inc/h/interlecto.hh"
+#endif
